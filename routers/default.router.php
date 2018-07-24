9a72cbce5e9319a12e48e56972d5e4997457c032
crab <crab68@126.com>
2018-07-24 20:00:47 +0800

    增加nginx配置，还有默认的错误页面

--- routers/default.router.php
+++ routers/default.router.php
@@ -17,3 +17,11 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     $json_data = ['status'=>true,'data'=>'welcome','desc'=>'welcome to crv','tag'=>$tag];
     return $response->withJson($json_data);
 });
+
+$app->get('/error', function (Request $request, Response $response, array $args) {
+    //$this->logger->info("Slim-Skeleton '/' index");
+    $tag = $request->getQueryParam('tag');
+    $tag = $tag ? $tag : 0;
+    $json_data = ['status'=>false,'data'=>'error','desc'=>'error happen','tag'=>$tag];
+    return $response->withJson($json_data);
+});
