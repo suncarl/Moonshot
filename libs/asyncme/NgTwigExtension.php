190acd083477fddfacc023225d289dd41af49eff
crab <crab68@126.com>
2018-07-27 02:00:20 +0800

    增加框架的模版扩展，并且加入测试方法

---
+++ libs/asyncme/NgTwigExtension.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiequan
+ * Date: 2018/7/27
+ * Time: 上午1:48
+ */
+
+namespace libs\asyncme;
+
+use \Twig_Extension;
+
+class NgTwigExtension extends Twig_Extension
+{
+
+    /**
+     * @var \Slim\Interfaces\RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var \Slim\Http\Uri
+     */
+    private $uri;
+
+    public function __construct($router, $uri)
+    {
+        $this->router = $router;
+        $this->uri = $uri;
+    }
+
+    public function getName()
+    {
+        return 'AsyncMe';
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('path_for', array($this, 'pathFor')),
+            new \Twig_SimpleFunction('base_url', array($this, 'baseUrl')),
+            new \Twig_SimpleFunction('async_me', array($this, 'asyncMe')),
+        ];
+    }
+
+    public function pathFor($name, $data = [], $queryParams = [], $appName = 'default')
+    {
+        return $this->router->pathFor($name, $data, $queryParams);
+    }
+
+    public function baseUrl()
+    {
+        if (method_exists($this->uri, 'getBaseUrl')) {
+            return $this->uri->getBaseUrl();
+        }
+    }
+
+    public function asyncMe($who)
+    {
+        return $this->getName().' '.$who;
+    }
+}
