5d5c5756178ee10cae29f8ef7d3814804481c394
crab <crab68@126.com>
2018-08-05 03:45:36 +0800

    增加应用中心的框架

--- admin/Plugins.php
+++ admin/Plugins.php
@@ -44,9 +44,8 @@ class Plugins extends PermissionBase
                 'data'=>'','category'=>'应用','placehold'=>'','use_priv'=>1,'type'=>1,
                 'link'=>1,'status'=>1,'name'=>'应用中心','icon'=>'th'
             ]
-
-
         ];
+        $plugin_menus = $this->recursion_menus($req,$plugin_menus);
 
         $data = [
             'default_frame_name'=>'仪表盘',
@@ -59,9 +58,59 @@ class Plugins extends PermissionBase
         return $this->render($status,$mess,$data,'template','plugin/index');
     }
 
+    /**
+     * 应用中心
+     * @param RequestHelper $req
+     * @param array $preData
+     */
+    public function centerAction(RequestHelper $req,array $preData)
+    {
+        $plugin_lists = $this->load_datas($req,$preData);
+        $status = true;
+        $mess = '成功';
+        $data = [
+            'plugin_lists'=>$plugin_lists,
+        ];
+
+        return $this->render($status,$mess,$data,'template','plugin/center');
+    }
+
     public function infoAction(RequestHelper $req,array $preData)
     {
-        echo 1;
         die();
+    }
+
+    /**
+     * @param RequestHelper $req
+     * @param array $preData
+     */
+    protected function load_datas(RequestHelper $req,array $preData)
+    {
+        $plugin_dir = '../plugins';
+        $plugins_lists = [];
+        if (is_dir($plugin_dir)) {
+            $fp = opendir($plugin_dir);
+            while($file = readdir($fp)) {
+                //去掉隐藏
+                if(substr($file,0,1)=='.') continue;
+
+                if ($plugin_dir.'/'.$file.'/info.xml') {
+                    $mtime = filemtime($plugin_dir.'/'.$file);
+                    $ctime = filectime($plugin_dir.'/'.$file);
+                    $plugins_lists[] = ['file'=>$file,'root'=>$plugin_dir,'ctime'=>$ctime,'mtime'=>$mtime];
+                }
+
+            }
+        }
+        $result = [];
+        if ($plugins_lists) {
+            foreach ($plugins_lists as $key=>$val) {
+                $xml = simplexml_load_file($val['root'].'/'.$val['file'].'/info.xml');
+                $json_xml=json_encode($xml);
+                $dejson_xml=json_decode($json_xml,true);
+                $result[] = array_merge($dejson_xml,$val);
+            }
+        }
+        return $result;
     }
 }
