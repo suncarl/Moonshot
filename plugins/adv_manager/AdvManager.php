8da3ade58a4780e3d7f2e70c60b1b6a8a28a51ed
crab <crab68@126.com>
2018-08-14 17:24:46 +0800

    修复插件菜单的bug

--- plugins/adv_manager/AdvManager.php
+++ plugins/adv_manager/AdvManager.php
@@ -8,8 +8,37 @@
 
 namespace plugins\adv_manager;
 
+use libs\asyncme\Plugins as Plugins;
+use libs\asyncme\RequestHelper as RequestHelper;
+use libs\asyncme\ResponeHelper as ResponeHelper;
+use \Slim\Http\UploadedFile;
 
 class AdvManager extends Plugins
 {
+    public function positionAction(RequestHelper $req,array $preData)
+    {
+        $status = true;
+        $mess = '成功';
+        $data = '<p><font style="color: red">position</font></p>';
 
+        return new ResponeHelper($status,$mess,$data,'html');
+    }
+
+    public function advAction(RequestHelper $req,array $preData)
+    {
+        $status = true;
+        $mess = '成功';
+        $data = '<p><font style="color: red">adv</font></p>';
+
+        return new ResponeHelper($status,$mess,$data,'html');
+    }
+
+    public function logAction(RequestHelper $req,array $preData)
+    {
+        $status = true;
+        $mess = '成功';
+        $data = '<p><font style="color: red">log</font></p>';
+
+        return new ResponeHelper($status,$mess,$data,'html');
+    }
 }
